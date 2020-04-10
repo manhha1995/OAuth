406cfbc373565709df56e187dacc53b07c9f8080
manhha1995 <61322614+manhha1995@users.noreply.github.com>
2020-04-10 23:01:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   facebook: {
     clentId: '249553676199264',
     clientSecret: 'a803fd5a28e386c58dcc0fcc45d952b6',
-    callbackURL: '',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
   },
 };
