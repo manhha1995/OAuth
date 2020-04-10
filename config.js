ef0d6f837369c84d0da63c4b418491e979780286
manhha1995 <61322614+manhha1995@users.noreply.github.com>
2020-04-10 22:59:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   facebook: {
-    clentId: '',
-    clientSecret: '',
+    clentId: '249553676199264',
+    clientSecret: 'a803fd5a28e386c58dcc0fcc45d952b6',
     callbackURL: '',
   },
 };
